708351a85b74036117f2047c1889c874df708545
Suleman Altaf <saksak218@gmail.com>
2025-06-09 18:55:33 +0500

    Implement Companion form UI

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,15 @@
+import CompanionForm from "@/components/CompanionForm";
+
 const NewCompanion = () => {
-  return <div>NewCompanion</div>;
+  return (
+    <main className="justify-center items-center py-10 min-lg:w-1/3 min-md:w-2/3">
+      <article className="flex flex-col gap-4 w-full">
+        <h1>Companion Builder</h1>
+
+        <CompanionForm />
+      </article>
+    </main>
+  );
 };
 
 export default NewCompanion;
