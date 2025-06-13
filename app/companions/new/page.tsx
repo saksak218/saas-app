528ec852292afa8fbb83215333a568b8187952d5
Suleman Altaf <saksak218@gmail.com>
2025-06-13 19:04:26 +0500

    Impliment Supabase and create companion

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,11 @@
 import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const NewCompanion = () => {
+const NewCompanion = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/sign-in");
+
   return (
     <main className="justify-center items-center py-10 min-lg:w-1/3 min-md:w-2/3">
       <article className="flex flex-col gap-4 w-full">
