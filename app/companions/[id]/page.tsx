cfad24903b0c8c34a63f0e122c7d1d4ddaec4940
Suleman Altaf <saksak218@gmail.com>
2025-06-27 20:37:03 +0500

    Setup Vapi, Make Companion header

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,5 +1,51 @@
-const CompanionSession = () => {
-  return <div>CompanionSession</div>;
+import { getCompanion } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+interface CompanionSessionPageProps {
+  params: Promise<{ id: string }>;
+}
+
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+  const { id } = await params;
+  const { name, subject, title, topic, duration } = await getCompanion(id);
+  const user = await currentUser();
+
+  if (!user) redirect("/sign-in");
+
+  if (!name) redirect("/companions");
+
+  return (
+    <main>
+      <article className="flex max-md:flex-col justify-between p-6 rounded-border">
+        <div className="flex items-center gap-2">
+          <div
+            className="max-md:hidden flex justify-center items-center rounded-lg size-[72px]"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <Image
+              src={`/icons/${subject}.svg`}
+              alt={subject}
+              width={35}
+              height={35}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <p className="font-bold text-2xl">{name}</p>
+              <div className="max-sm:hidden subject-badge">{subject}</div>
+            </div>
+            <p className="text-lg">{topic}</p>
+          </div>
+        </div>
+        <div className="max-md:hidden items-start text-2xl">
+          {duration} minutes
+        </div>
+      </article>
+    </main>
+  );
 };
 
 export default CompanionSession;
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -47,3 +47,18 @@ export const getAllCompanions = async ({
 
   return companions;
 };
+
+export const getCompanion = async (id: string) => {
+  const supabase = createSupabaseClient();
+
+  const { data, error } = await supabase
+    .from("companions")
+    .select()
+    .eq("id", id);
+
+  if (error) {
+    console.log(error);
+  }
+
+  return data[0];
+};
